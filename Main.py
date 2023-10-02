4bb4f8029fb74612d71a3056ae02a1a2ff16a016
Carlos Mitran <100472086@alumnos.uc3m.es>
2023-10-02 21:06:42 +0200

    Arreglado invalid key, añadido incorrect password funcional, y creacion de usuario con keys

--- Main.py
+++ Main.py
@@ -2,18 +2,15 @@ from tkinter import *
 from tkinter import ttk
 import json
 import os
+
+import cryptography.exceptions
 from cryptography.hazmat.primitives.kdf.scrypt import Scrypt
 import base64
 import re
 
 
-
 def write_input(json_file, inputs):
     list1 = open_json(json_file)
-    found = find_username(list1, inputs)
-    if found:
-        print("Welcome!, " + inputs["username"])
-        return True
     list1.append(inputs)
     try:
         with open(json_file, "w", encoding="UTF-8", newline="") as file:
@@ -34,27 +31,29 @@ def open_json(json_file):
     return data_list
 
 
-
-def find_username(data_list, inputs):
+def create_user(data_list, inputs):
     for item in data_list:
         if item["username"] == inputs["username"]:
-            print("\ninputpwd: " + inputs["password"] + "\n")
-            if verify_key(inputs["password"], item["password"], item["salt"]):
+            try:
+                verify_key(inputs["password"], item["password"], item["salt"])
+                return True
+
+                # raise Exception("Incorrect password")
+            # print("Welcome!, " + inputs["username"]) lo quito porque sino se imprime 3 veces
+            except cryptography.exceptions.InvalidKey:
                 incorrectPasswordLabel = Label(root, text="Incorrect password", font='Century 12', fg="#FF5733")
                 incorrectPasswordLabel.pack()
                 passwordBox.delete(0, END)
-                #raise Exception("Incorrect password")
-            # print("Welcome!, " + inputs["username"]) lo quito porque sino se imprime 3 veces
-            newsalt = os.urandom(16)
-            saltascii = encode_to_ascii(newsalt)
-            item["salt"] = saltascii
-            return True
-    return False
+                return False
+    adduser(inputs)
 
+def adduser(data_list):
+    newsalt = os.urandom(16)
+    key, saltascii = calculate_key(data_list["password"], newsalt)
+    create_dict(data_list["username"], key, saltascii)
 
 def create_dict(user, password, salt):
     user_list = {"username": user, "password": password, "salt": salt}
-    print(user_list)
     write_input("test.json", user_list)
 
 
@@ -67,35 +66,14 @@ def get_values():
     print(user)
     password = passwordBox.get()
     print(password)
-    salt = os.urandom(16)
-    password, saltascii = calculate_key(password, salt)
-    create_dict(user, password, saltascii)
-    if find_username(open_json("test.json"), {"username": user, "password": password, "salt": saltascii}):
+    if create_user(open_json("test.json"), {"username": user, "password": password}):
         root.geometry("1500x950")
         for widget in root.winfo_children():
             widget.destroy()
         welcomeLabel = Label(root, text="Welcome! " + user, font=('Century 20 bold'))
         welcomeLabel.place(x=25, y=25)
 
-space1 = Label(root, text=" ")
-space1.pack(pady=10)
-title = Label(root, text="Log in:", font=('Century 20 bold'))
-title.pack(pady=30)
-userLabel = ttk.Label(root, text="Username:", font=('Century 12'))
-userLabel.pack()
-userBox = ttk.Entry(root, font=('Century 12'), width=40)
-userBox.pack()
-space2 = Label(root, text=" ")
-space2.pack(pady=5)
-passwordLabel = ttk.Label(root, text="Password:", font=('Century 12'))
-passwordLabel.pack()
-passwordBox = ttk.Entry(root, show= '*', font=('Century 12'), width=40)
-passwordBox.pack()
-loginWarning = Label(root, text="(If user does not exist, it will be created)", font=('Century 12 italic'))
-loginWarning.pack(pady=10)
 
-loginButton = ttk.Button(root, text="Log in", command=get_values)
-loginButton.pack()
 
 def calculate_key(inputs, salt):
     kdf = Scrypt(
@@ -114,6 +92,7 @@ def calculate_key(inputs, salt):
     print(saltascii)
     return keyascii, saltascii
 
+
 def verify_key(password, key, salt):
     salt = decode_to_bytes(salt)
     kdf = Scrypt(
@@ -123,34 +102,46 @@ def verify_key(password, key, salt):
         r=8,
         p=1,
     )
-    key64 = base64.b64encode(bytes(key, 'utf-8'))
-    keybytes = base64.b64decode(key64)
-    pwd = password.encode()
-    print("pwd: " + password)
-    print(password.encode())
-    print("key: " + key)
-    print(salt)
-    print(key64)
-    print(keybytes)
-    kdf.verify(pwd, keybytes)
+    password = bytes(password, "utf-8")
+    keybytes = decode_to_bytes(key)
+    kdf.verify(password, keybytes)
 
     return True
 
 
 def encode_to_ascii(key):
     key64 = base64.b64encode(key)
-    keyascii = str(key64.decode("utf-8"))
+    keyascii = key64.decode("ascii")
 
     return keyascii
 
+
 def decode_to_bytes(key):
-    key64 = key.encode('utf-8')
+    keybytes1 = base64.b64decode(key)
+    key64 = base64.b64encode(keybytes1)
     bytekey = base64.b64decode(key64)
 
     return bytekey
 
 
+space1 = Label(root, text=" ")
+space1.pack(pady=10)
+title = Label(root, text="Log in:", font=('Century 20 bold'))
+title.pack(pady=30)
+userLabel = ttk.Label(root, text="Username:", font=('Century 12'))
+userLabel.pack()
+userBox = ttk.Entry(root, font=('Century 12'), width=40)
+userBox.pack()
+space2 = Label(root, text=" ")
+space2.pack(pady=5)
+passwordLabel = ttk.Label(root, text="Password:", font=('Century 12'))
+passwordLabel.pack()
+passwordBox = ttk.Entry(root, show='*', font=('Century 12'), width=40)
+passwordBox.pack()
+loginWarning = Label(root, text="(If user does not exist, it will be created)", font=('Century 12 italic'))
+loginWarning.pack(pady=10)
 
-
+loginButton = ttk.Button(root, text="Log in", command=get_values)
+loginButton.pack()
 
 root.mainloop()
