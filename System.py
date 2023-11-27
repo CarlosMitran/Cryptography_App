6b0c570ca96122f6afda082a046e9b6862295a83
CarlosMitran <100472086@alumnos.uc3m.es>
2023-11-27 18:40:25 +0100

    parte 4

--- System.py
+++ System.py
@@ -3,7 +3,6 @@ from tkinter import *
 from tkinter import ttk
 import json
 import os
-
 import cryptography.exceptions
 from cryptography.hazmat.primitives.kdf.scrypt import Scrypt
 from cryptography.hazmat.primitives.ciphers.aead import ChaCha20Poly1305
@@ -17,8 +16,7 @@ import re
 import time
 from cryptography import x509
 from cryptography.x509.oid import NameOID
-from cryptography.hazmat.primitives import hashes
-
+import shutil
 
 def certificar():
     destroy_widgets()
@@ -65,14 +63,14 @@ def validarPass(doctorName, pacient):
     passwordBox = ttk.Entry(root, show='*', font=('Century 12'), width=40)
     passwordBox.pack()
     loginButton = ttk.Button(root, text="Log in",
-                             command=lambda: generarcertificados(doctorName, str(passwordBox.get()), pacient))
+                             command=lambda: generarCSR(doctorName, str(passwordBox.get()), pacient))
     loginButton.pack()
 
 
-def generarcertificados(doctorName, password, pacient):
+def generarCSR(doctorName, password, pacient):
     passbytes = bytes(password, "utf-8")
     with open(doctorName + ".txt", 'rb') as file:
-    # Read the entire content of the file
+        # Read the entire content of the file
         key = serialization.load_pem_private_key(
             file.read(),
             password=passbytes,
@@ -112,35 +110,69 @@ def generarcertificados(doctorName, password, pacient):
 
     ).sign(key, hashes.SHA256())
 
-    # Write our CSR out to disk.
+    # Write our CERT out to disk.
 
-    with open("CSR/certificado" + pacient + ".pem", "wb") as f:
+    with open("AC1/solicitudes/CSR" + pacient + ".pem", "wb") as f:
         f.write(csr.public_bytes(serialization.Encoding.PEM))
 
-    readcert(pacient)
+    createcert(pacient, doctorName)
 
 
-def readcert(username):
-    with open("ac1cert.pem", "rb") as f:
+def createcert(pacient, doctorName):
+    with open("AC1/serial", "r") as f:
+        num = f.read()
+
+    os.chdir("AC1")
+    os.system("ls")
+    os.system("openssl req -in ./solicitudes/CSR" + pacient + ".pem -text -noout")
+    os.system("openssl ca -in ./solicitudes/CSR" + pacient + ".pem -notext -config ./openssl_AC1.cnf")
+    os.chdir("..")
+    source_path = "AC1/nuevoscerts/"+num[0]+num[1]+".pem"
+    print(source_path)
+    destination_path = "CERT/CERT"+pacient+".pem"
+    # Copy the file
+    shutil.copyfile(source_path, destination_path)
+    readcert(pacient, doctorName)
+
+
+def readcert(username, doctorName):
+    title = Label(root, text="CSR creado, introduzca la clave de certificado", font=('Century 20 bold'))
+    title.pack(pady=30)
+    space2 = Label(root, text=" ")
+    space2.pack(pady=5)
+    passwordLabel = ttk.Label(root, text="Contraseña:", font=('Century 12'))
+    passwordLabel.pack()
+    passwordBox = ttk.Entry(root, show='*', font=('Century 12'), width=40)
+    passwordBox.pack()
+    loginButton = ttk.Button(root, text="Log in",
+                             command=lambda: validarCert(str(passwordBox.get()), username))
+    loginButton.pack()
+
+
+def validarCert(password, username):
+    with open("CERT/" + "CERT" + username + ".pem", "rb") as f:
         pem_data = f.read()
+    with open("AC1/privado/ca1key.pem", 'rb') as file:
+        # Read the entire content of the file
+        private_key = serialization.load_pem_private_key(
+            file.read(),
+            password= bytes(password, "utf-8"),
+        )
+    public_key = private_key.public_key()
     cert = x509.load_pem_x509_certificate(pem_data)
-    public_key = cert.public_key()
-    signatureload = cert.signature
-    with open(username + "signedData" + ".txt", "r") as file:
-        messageload = file.read()
-
-    messageloadbytes = bytes(messageload, "utf-8")
-
-    public_key.verify(
-        signatureload,
-        messageloadbytes,
-        padding.PSS(
-            mgf=padding.MGF1(hashes.SHA256()),
-            salt_length=padding.PSS.MAX_LENGTH
-        ),
-        hashes.SHA256()
-    )
-    print("El certificado ha sido verificado")
+    try:
+        public_key.verify(
+            cert.signature,
+            cert.tbs_certificate_bytes,
+            padding.PKCS1v15(),
+            cert.signature_hash_algorithm,
+        )
+        CorrectPasswordLabel = Label(root, text="Certificado verificado correctamente", font='Century 12')
+        CorrectPasswordLabel.pack()
+        return True
+    except ValueError:
+        incorrectPasswordLabel = Label(root, text="Incorrect password", font='Century 12', fg="#FF5733")
+        incorrectPasswordLabel.pack()
 
 
 def destroy_widgets():
